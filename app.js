7a3073335c0b548347b7ee64f1cfaa732fef6382
zSqualler <john199502031@gmail.com>
2024-12-14 15:02:10 +0100

    refactor(basic-3.1, 3.2; extended-2.1): add single style for selectors naming, add meaningful naming of identifiers and classes, using BEM

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("tasks__incomplete");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("tasks__completed");//completed-tasks
 
 
 //New task list item
@@ -33,18 +33,19 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className='list__item-label';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task__input";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="list__item-button_edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="list__item-button_delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.className='list__item-img';
   deleteButton.appendChild(deleteButtonImg);
 
 
@@ -83,13 +84,13 @@ var editTask=function(){
   var listItem=this.parentNode;
 
   var editInput=listItem.querySelector('input[type=text]');
-  var label=listItem.querySelector("label");
-  var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("editMode");
-  //If class of the parent is .editmode
+  var label=listItem.querySelector(".list__item-label");
+  var editBtn=listItem.querySelector(".list__item-button_edit");
+  var containsClass=listItem.classList.contains("list__item--active");
+  //If class of the parent is .list__item--active
   if(containsClass){
 
-    //switch to .editmode
+    //switch to .list__item--active
     //label becomes the inputs value.
     label.innerText=editInput.value;
     editBtn.innerText="Edit";
@@ -98,8 +99,8 @@ var editTask=function(){
     editBtn.innerText="Save";
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  //toggle .list__item--active on the parent.
+  listItem.classList.toggle("list__item--active");
 };
 
 
@@ -156,8 +157,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
 //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var editButton=taskListItem.querySelector(".list__item-button_edit");
+  var deleteButton=taskListItem.querySelector(".list__item-button_delete");
 
 
   //Bind editTask to edit button.
